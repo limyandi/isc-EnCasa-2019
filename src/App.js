b54ceef7ed3da4f9c72906e4bfb2ef929a573e16
rico <limyandi@gmail.com>
2019-08-21 15:01:45 +1000

    commit change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Containers/routes';
--- src/Containers/HomeView.js
+++ src/Containers/HomeView.js
@@ -2,13 +2,19 @@ import React from 'react';
 import axios from 'axios';
 
 function HomeView() {
+  // TODO: or persist data
+  const [userMode, setUserMode] = React.useState('customer');
+
   const axiosFetch = () =>
     axios.get('/api/users').then(res => console.log(res));
 
   return (
-    <button type="button" onClick={axiosFetch}>
-      AH
-    </button>
+    <div>
+      <button type="button" onClick={axiosFetch}>
+        Try Get Value
+      </button>
+      <div>Your delivery is here!</div>
+    </div>
   );
 }
 
