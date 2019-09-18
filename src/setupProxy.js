55cf88de7c7d99d6f08e002a12b6b3dc9d2ca39a
lvicotrico <limyandi@gmail.com>
2019-09-18 22:16:27 +1000

    add environment variable

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,12 @@
 const proxy = require('http-proxy-middleware');
+require('dotenv').config();
 
 module.exports = function(app) {
   // app.use(proxy('/api', { target: 'http://localhost:5000' }));
   app.use(
-    proxy(['/logistics', '/fileserver'], { target: 'http://localhost:52776' })
+    proxy(['/logistics', '/fileserver'], {
+      target: `http://localhost:${process.env.IRIS_PORT}`
+    })
   );
   app.use(proxy('/notif', { target: 'http://localhost:9999' }));
 };
